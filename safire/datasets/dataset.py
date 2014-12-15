5efcf16d521e74ea888528ce0f827175f0e297de
jan.hajic.jr <hajicj@gmail.com>
2014-12-15 13:03:15 +0100

    WIP: Word2VecSamplingDatasetTransformer -- towards training on image-word pairs.

--- safire/datasets/dataset.py
+++ safire/datasets/dataset.py
@@ -1,7 +1,4 @@
-import cPickle
 from gensim.interfaces import TransformationABC
-import numpy
-import theano
 from safire.datasets.unsupervised_dataset import UnsupervisedDataset
 
 
@@ -118,7 +115,8 @@ class DatasetTransformer(TransformationABC):
         :type batch: numpy.array, theano.shared
         :param batch: A batch.
 
-        :return:
+        :return: The transformed batch. If a dataset is given instead of
+            a batch, applies the transformer and returns a TransformedDataset.
         """
         if isinstance(batch, Dataset):
             return self._apply(batch)
@@ -135,93 +133,3 @@ class DatasetTransformer(TransformationABC):
         return transformed_dataset
 
 
-class Word2VecSamplingDatasetTransformer(DatasetTransformer):
-    """A DatasetTransformer that samples a word from each document vector
-    and returns its word2vec representation instead.
-    """
-    def __init__(self, w2v_transformer, embeddings_matrix=None,
-                 pickle_embeddings_matrix=None):
-        """Initializes the transformer -- specifically the word2vec embeddings.
-
-        :param w2v_transformer: A Word2Vec transformer that provides the
-            embeddings dict and id2word mapping. Used during building the
-            embeddings matrix; NOT saved. Can be None, if ``embeddings_matrix``
-            is provided.
-
-            The id2word mapping of the transformer is expected to map **the
-            indices of the dataset on which the transformer will run** to
-            words. This is important after a FrequencyBasedTransformer is
-            applied.
-        """
-        self.embeddings_matrix = None
-        self.n_out = None
-        if embeddings_matrix:
-            self.embeddings_matrix = cPickle.load(embeddings_matrix)
-        else:
-            self.embeddings_matrix = self.build_matrix(w2v_transformer)
-
-        self.n_out = self.embeddings_matrix.shape[1]
-
-        if pickle_embeddings_matrix:
-            with open(pickle_embeddings_matrix, 'wb') as phandle:
-                cPickle.dump(self.embeddings_matrix, phandle, protocol=-1)
-
-        self.n_out = len(self.embeddings[self.embeddings.keys()[0]])
-
-        # Build mapping matrix
-        self.embeddngs_matrix = self.build_mapping()
-
-    def __getitem__(self, batch):
-        """Transforms the given batch.
-
-        :param batch:
-        :return:
-        """
-
-        # Sample from each document (batch row) one word (column).
-        batch_projection = self.get_batch_sample(batch)
-
-        # Use embedding of given word as document vector.
-        # - batch_projection is X * n_in,
-        # - embeddings_matrix is n_in * n_out
-        embeddings = theano.tensor.dot(batch_projection,
-                                       self.embeddings_matrix)
-        return embeddings
-
-    def build_matrix(self, w2v_transformer):
-        """Uses the information in a w2v_transformer to build an embeddings
-        matrix that speeds up ``__getitem__`` computation.
-
-        :type w2v_transformer: safire.data.word2vec_transformer.Word2VecTransformer
-        :param w2v_transformer: A Word2Vec transformer that provides the
-            embeddings dict and id2word mapping. Used during building the
-            embeddings matrix; NOT saved. Can be None, if ``embeddings_matrix``
-            is provided.
-
-            The id2word mapping of the transformer is expected to map **the
-            indices of the dataset on which the transformer will run** to
-            words. This is important after a FrequencyBasedTransformer is
-            applied.
-
-        :return: A theano shared variable with the embeddings matrix.
-            The matrix dimensions are ``n_in * n_out``, where ``n_in`` is the
-            input document vector dimension and ``n_out`` is the embedding
-            dimension.
-
-            .. warning::
-
-                We assume the id2word mapping is dense: all positions are
-                used. Furthermore, we assume that embedding with ID ``x``
-                corresponds to the ``x``-th column of an input batch.
-        """
-        n_in = len(w2v_transformer.id2word)
-        n_out = w2v_transformer.n_out
-        matrix = numpy.zeros((n_in, n_out))
-
-        id2word = w2v_transformer.id2word
-
-        for wid in xrange(len(id2word)):
-            embedding = w2v_transformer[(wid, 1)]
-            matrix[wid, :] = embedding
-
-        return matrix
